0f3cc3eea53e410fe36ad3001b7db2b238e3bcf7
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-10-11 16:06:50 +0200

    scrollspy prototype working

--- scrollspy.js
+++ scrollspy.js
@@ -1,5 +1,5 @@
 let section = document.querySelectorAll('section'); // get all <section> elements
-let menuLinks = document.querySelectorAll('header menu a'); // get all <a> inside <menu> inside <header> element
+let menuLinks = document.querySelectorAll('div menu a'); // get all <a> inside <menu> inside <header> element
 let sectionHeight = document.querySelector('section').offsetHeight; // get the height of the top menu
 let sectionStartingPointArray = []; // array to store the top-most pixel points of each <section>
 let menuLinksArray = []; // array to store the menu elements
@@ -14,12 +14,18 @@ menuLinks.forEach((menuLink) => {menuLinksArray.push(menuLink);});
 window.onscroll = () => {
 	// amount of pixel we have scrolled downwards from the top-most point of the web page
 	let downwardScroll = window.scrollY;
+	//console.log("downwardScroll: ", downwardScroll);
+	//console.log("section: ", section);
+	//console.log("menuLinksArray: ", menuLinksArray);
+
 
 	// for every <section>'s top-most point
 	sectionStartingPointArray.forEach((sectionStart, sectionIndex) => {
 		// check if the current downward scroll is on the middle of each <section>
 		// by using its top-most point minus its height divide by half
 		if (downwardScroll >= sectionStart - sectionHeight / 2) {
+			console.log("sectionStart: ", sectionStart);
+			console.log("menuLinksArray[sectionIndex].classList: ", menuLinksArray[sectionIndex].classList);
 			//if so, we remove all the 'active' classes on all menu links
 			menuLinksArray.forEach((menu) => {menu.classList.remove('active');});
 			//then add the 'active' class on the corresponding menu depending on which section the scroll is currently at
