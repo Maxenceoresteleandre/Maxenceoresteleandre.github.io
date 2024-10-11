3335055b360d9d8522d11ae2cc8d8ee4fc36594d
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-10-11 22:40:35 +0200

    test with collapsible work item

--- work_item_script.js
+++ work_item_script.js
@@ -6,10 +6,10 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.maxHeight){
+    if (content.style.maxHeight > "0px") {
       content.style.maxHeight = null;
     } else {
-      content.style.maxHeight = content.scrollHeight + "px";
+      content.style.maxHeight = "999px";
     } 
   });
 }
